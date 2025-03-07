e05b88ec49f36f249acb5c0c8a073a6e370323c7
Manuel Trinidad <kinowashere@proton.me>
2025-03-07 18:26:42 +0200

    spotify auth link generator

---
+++ src/spotify/auth/keys.ts
@@ -0,0 +1,4 @@
+import { safeEnv } from "../../utils/safeEnv";
+
+export const SPOTIFY_CLIENT_ID = safeEnv("SPOTIFY_CLIENT_ID");
+export const SPOTIFY_CLIENT_SECRET = safeEnv("SPOTIFY_CLIENT_SECRET");
---
+++ src/spotify/authLink.ts
@@ -0,0 +1,31 @@
+import { safeEnv } from "../utils/safeEnv";
+import { SPOTIFY_CLIENT_ID } from "./auth/keys";
+
+const AUTH_CALLBACK_PATH = "/auth_callback";
+const AUTH_CALLBACK_SERVER_PORT = safeEnv("AUTH_CALLBACK_SERVER_PORT");
+const RESPONSE_TYPE = "code";
+
+type GetAuthLinkProps = {
+  scope?: string;
+  state?: string;
+  showDialog?: boolean;
+};
+
+export const getAuthLink = ({ scope, state, showDialog }: GetAuthLinkProps) => {
+  const url = new URL("https://accounts.spotify.com/authorize");
+  url.searchParams.append("redirect_uri", generateRedirectUri());
+  url.searchParams.append("show_dialog", showDialog ? "true" : "false");
+  url.searchParams.append("client_id", SPOTIFY_CLIENT_ID);
+  url.searchParams.append("response_type", RESPONSE_TYPE);
+  if (state) {
+    url.searchParams.append("state", state);
+  }
+  if (scope) {
+    url.searchParams.append("scope", scope);
+  }
+  return url.toString();
+};
+
+export const generateRedirectUri = () => {
+  return `http://localhost:${AUTH_CALLBACK_SERVER_PORT}${AUTH_CALLBACK_PATH}`;
+};
