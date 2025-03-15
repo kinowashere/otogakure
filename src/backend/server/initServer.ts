de702340394d1fc3755259bd9506f2e616407e7f
Manuel Trinidad <kinowashere@proton.me>
2025-03-15 18:59:49 +0200

    chart endpoint

--- src/backend/server/initServer.ts
+++ src/backend/server/initServer.ts
@@ -1,3 +1,5 @@
+import { statsChartsRepo } from "../../repo/charts";
+import { StatsChartType } from "../../statsChart/statsChart";
 import { safeEnv } from "../../utils/safeEnv";
 
 const WEB_SERVER_PORT = safeEnv("WEB_SERVER_PORT");
@@ -7,6 +9,21 @@ export const initServer = () =>
     routes: {
       "/": () => {
         return new Response("Hello, world!", { status: 200 });
+      },
+      "/api/chart/:type": async (req) => {
+        /**
+         * this will reply with a chart's data by it's id
+         **/
+        const type = req.params.type as StatsChartType;
+
+        const chart = await statsChartsRepo().findOne({
+          type: type,
+        });
+
+        if (!type || !Object.values(StatsChartType).includes(type) || !chart) {
+          return new Response("not found", { status: 404 });
+        }
+        return Response.json({ chart }, { status: 200 });
       },
     },
     port: WEB_SERVER_PORT,
