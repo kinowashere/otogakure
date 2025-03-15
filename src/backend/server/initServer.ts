d5ebe709e7d8f3b35bba90c150c28f0ce4d14dbb
Manuel Trinidad <kinowashere@proton.me>
2025-03-15 19:21:18 +0200

    typed api

--- src/backend/server/initServer.ts
+++ src/backend/server/initServer.ts
@@ -1,6 +1,7 @@
 import { statsChartsRepo } from "../../repo/charts";
 import { StatsChartType } from "../../statsChart/statsChart";
 import { safeEnv } from "../../utils/safeEnv";
+import { failure, success } from "./api";
 
 const WEB_SERVER_PORT = safeEnv("WEB_SERVER_PORT");
 
@@ -11,19 +12,27 @@ export const initServer = () =>
         return new Response("Hello, world!", { status: 200 });
       },
       "/api/chart/:type": async (req) => {
-        /**
-         * this will reply with a chart's data by it's id
-         **/
-        const type = req.params.type as StatsChartType;
+        try {
+          /**
+           * this will reply with a chart's data by it's id
+           **/
+          const type = req.params.type as StatsChartType;
 
-        const chart = await statsChartsRepo().findOne({
-          type: type,
-        });
+          const chart = await statsChartsRepo().findOne({
+            type: type,
+          });
 
-        if (!type || !Object.values(StatsChartType).includes(type) || !chart) {
-          return new Response("not found", { status: 404 });
+          if (
+            !type ||
+            !Object.values(StatsChartType).includes(type) ||
+            !chart
+          ) {
+            return Response.json(failure(), { status: 404 });
+          }
+          return Response.json(success(chart), { status: 200 });
+        } catch (err) {
+          return Response.json(failure(err?.toString()));
         }
-        return Response.json({ chart }, { status: 200 });
       },
     },
     port: WEB_SERVER_PORT,
